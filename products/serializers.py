b1a987bef6dd84d474c37f686451241c372b86d7
Rijin NP <rijinswaminathan@gmail.com>
2021-03-16 10:48:11 +0530

    change in response.

--- products/serializers.py
+++ products/serializers.py
@@ -15,7 +15,7 @@ class ViewCategorySerializer(serializers.Serializer):
     product = serializers.SerializerMethodField()
 
     def get_category_name(self, obj):
-        category_name = model.Category.objects.filter(parent_cat__gte=obj.id)
+        category_name = model.Category.objects.filter(parent_cat=obj.id)
         if category_name:
             return obj.category_name
         return obj.category_name
@@ -25,14 +25,14 @@ class ViewCategorySerializer(serializers.Serializer):
         :param obj:
         :return: Get the product details according to category id
         """
-        category_name = model.Category.objects.filter(parent_cat__gte=obj.id)
+        category_name = model.Category.objects.filter(parent_cat=obj.id)
 
         if category_name:
-            product_data = model.Product.objects.filter(
-                category_id__category_name__contains=category_name[0])
-            product_ser = ViewShopProductSerializer(product_data, many=True)
-            return product_ser.data
-
+            for i in range(len(category_name)):
+                product_data = model.Product.objects.filter(
+                    category_id__category_name__contains=category_name[i])
+                product_ser = ViewShopProductSerializer(product_data, many=True)
+                return product_ser.data
         products = model.Product.objects.filter(category_id=obj.id)
         product_ser = ViewShopProductSerializer(products, many=True)
         return product_ser.data
@@ -49,15 +49,9 @@ class MediaSerializer(serializers.ModelSerializer):
 
 
 class ViewShopProductSerializer(serializers.Serializer):
-    id = serializers.SerializerMethodField()
-    product_name = serializers.SerializerMethodField()
+    id = serializers.IntegerField()
+    product_name = serializers.CharField()
     product_image = serializers.SerializerMethodField()
-
-    def get_id(self, obj):
-        return obj.id
-
-    def get_product_name(self, obj):
-        return obj.product_name
 
     def get_product_image(self, obj):
         """
